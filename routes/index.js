3d31d4f318abaa35988f8f632ee4fa4cef6345f1
weple <weple@qq.com>
2016-12-25 23:56:27 +0800

    remove the mongodb start code

--- routes/index.js
+++ routes/index.js
@@ -4,11 +4,11 @@ var client = require('./Client/index.js'),
 
 module.exports = function(app)
 {
-	exec('"D:\\Program Files (x86)\\mongodb-3.0.7\\bin\\mongod" --dbpath "D:\\Program Files (x86)\\mongodb-3.0.7\\data"', function(err, stdout, stderr)
-	{
-		if (err) throw err;
-		console.log(stdout);
-	});
+	// exec('"D:\\Program Files (x86)\\mongodb-3.0.7\\bin\\mongod" --dbpath "D:\\Program Files (x86)\\mongodb-3.0.7\\data"', function(err, stdout, stderr)
+	// {
+	// 	if (err) throw err;
+	// 	console.log(stdout);
+	// });
 	client(app);
 
 	//server(app);
